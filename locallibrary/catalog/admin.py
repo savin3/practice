0f13f9313bd305ac15d423d644136485a19ed26a
savin ilya <savin15ilya3112@gmail.com>
2025-11-12 13:34:13 +0700

    fix added the language field to the book model and migrated it to the db

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -47,13 +47,10 @@ class Book(models.Model):
     """
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file.
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
 
@@ -67,6 +64,11 @@ class Book(models.Model):
 
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
 
 import uuid
 

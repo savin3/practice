6b72167eec391d3e794c5a605a1f93b4884a4cf1
savin ilya <savin15ilya3112@gmail.com>
2025-11-13 11:06:55 +0700

    added sessions

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -4,6 +4,11 @@ from .models import Book, Author, BookInstance, Genre
 
 def index(request):
 
+    num_authors = Author.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
@@ -29,7 +34,13 @@ def index(request):
     }
 
     # Render the html template index.html with the data in the context variable
-    return render(request, 'index.html', context)
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},  # num_visits appended
+    )
 
 from django.views import generic
 

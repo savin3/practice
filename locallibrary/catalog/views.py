8a77c3e24d4372a8be7d16325d428be785788e9e
savin ilya <savin15ilya3112@gmail.com>
2025-11-13 10:14:44 +0700

    added display of the author list page and the detailed information page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -40,18 +40,9 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
 
-    ''' class Author(models.Model):
-        first_name = models.CharField(max_length=100)
-        last_name = models.CharField(max_length=100)
-        date_of_birth = models.DateField(null=True, blank=True)
-        date_of_death = models.DateField('Died', null=True, blank=True)
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
 
-        def get_absolute_url(self):
-            return reverse('author-detail', args=[str(self.id)])
-
-        def __str__(self):
-            return '%s, %s' % (self.last_name, self.first_name)
-
-        class Meta:
-            ordering = ['last_name'] 
-    '''
+class AuthorDetailView(generic.DetailView):
+    model = Author

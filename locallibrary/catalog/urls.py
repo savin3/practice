0dc550b0f72d8ed1bbc68b37b4631aa905419a70
savin ilya <savin15ilya3112@gmail.com>
2025-11-13 09:20:25 +0700

    created a page display with a list of books and detailed information about each book

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
